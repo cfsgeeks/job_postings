e1bff369f25c2fc13703f61b6cdce281ea82ab86
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-04-24 10:56:07 -0500

    Model and view changes

--- models.py
+++ models.py
@@ -23,21 +23,21 @@ class Job(models.Model):
 
 	status = models.CharField(max_length='1',choices=STATUS_CHOICES)
 	publish_date = models.DateField()
-	closing_date = models.DateField()
+	closing_date = models.DateField(blank=True)
 	title = models.CharField(max_length=64, blank=False)
 	location = models.ManyToManyField(Location)
 	term = models.CharField(max_length=2,choices=TERM_CHOICES)
-	contract_length = models.IntegerField(max_length=2, help_text='Length specified in months')
+	contract_length = models.IntegerField(max_length=2, blank=True, help_text='Length specified in months')
 	extension_possible = models.BooleanField()
 	union = models.BooleanField()
-	positions_available = models.IntegerField()
-	qualifications = models.TextField()
-	requirements = models.TextField()
-	service = models.CharField(max_length=64)
+	positions_available = models.IntegerField(default=1)
+	qualifications = models.TextField(blank=True)
+	requirements = models.TextField(blank=True)
+	service = models.CharField(max_length=64, blank=True)
 	payscale_start = models.DecimalField(max_digits=8, decimal_places=2)
 	payscale_end = models.DecimalField(max_digits=8, decimal_places=2)
 	payscale_period = models.CharField(max_length=1,choices=WAGE_CHOICES)
-	hours_per_week = models.DecimalField(max_digits=4,decimal_places=2)
+	hours_per_week = models.DecimalField(max_digits=4,decimal_places=2, help_text='Enter in decimal form. Example: 33 & 3/4 hours is 33.75')
 
 	def clean(self):
 		if self.term == 'CF' or self.term == 'CP':
--- views.py
+++ views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from job_postings.models import SalariedJob, NonSalariedJob
+from job_postings.models import Job
 import datetime
 
 TODAY = datetime.date.today()
 
 def index(request):
-	salaried_jobs_list = SalariedJob.objects.filter(status='P',closing_date__gte=TODAY).order_by('-publish_date')
-	nonsalaried_jobs_list = NonSalariedJob.objects.filter(status='P',closing_date__gte=TODAY).order_by('-publish_date')
-	context = {'jobs':salaried_jobs_list}
-	context[jobs].update(nonsalaried_jobs_list)
+	jobs_list = Job.objects.filter(status='P',closing_date__gte=TODAY).order_by('-publish_date')
+	context = {'jobs':jobs_list}
 	return render(request,'listings.html',context)
