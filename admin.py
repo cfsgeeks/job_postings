b7209941bba1f1f05869ec4d4717cca8a793d252
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-04-24 10:21:08 -0500

    Hide Job Location from Menu

--- admin.py
+++ admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from job_postings.models import Job, Location
 
+class LocationAdmin(admin.ModelAdmin):
+	def get_model_perms(self,request):
+		return {}
+
 admin.site.register(Job)
-admin.site.register(Location)
+admin.site.register(Location,LocationAdmin)
