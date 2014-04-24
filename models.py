f2c000884682a8896c9f012ecd893bff33837151
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-04-24 09:48:35 -0500

    Changed to a MTM

--- models.py
+++ models.py
@@ -8,24 +8,33 @@ MONTH = TODAY.month
 YEAR = TODAY.year
 OPEN = datetime.date((YEAR+5),12,31)
 
+class Location(models.Model):
+	name = models.CharField(max_length=64)
+
 class Job(models.Model):
 
 	STATUS_CHOICES = (('D','Draft'),('P','Published'))
 	LOCATION_CHOICES = (('ALL','All'), ('ATI','Atikokan'), ('DRY','Dryden'),('FFR','Fort Frances'),('KEN','Kenora'),('RLK','Red Lake'),('SLK','Sioux Lookout'))
 	TERM_CHOICES = (('PT','Part-Time'),('FT','Full-Time'),('CF','Contact Full-Time'),('CP','Contract Part-Time'))
+	WAGE_CHOICES = (('A','Per Annum'),('H','Per Hour'),('W','Per Week'))
 
 	status = models.CharField(max_length='1',choices=STATUS_CHOICES)
 	publish_date = models.DateField()
 	closing_date = models.DateField()
 	title = models.CharField(max_length=64, blank=False)
-	location = models.CharField(max_length=3,choices=LOCATION_CHOICES)
+	location = models.ManyToManyField(Location)
 	term = models.CharField(max_length=2,choices=TERM_CHOICES)
 	contract_length = models.IntegerField(max_length=2, help_text='Length specified in months')
 	extension_possible = models.BooleanField()
 	union = models.BooleanField()
 	positions_available = models.IntegerField()
 	qualifications = models.TextField()
+	requirements = models.TextField()
 	service = models.CharField(max_length=64)
+	payscale_start = models.DecimalField(max_digits=8, decimal_places=2)
+	payscale_end = models.DecimalField(max_digits=8, decimal_places=2)
+	payscale_period = models.CharField(max_length=1,choices=WAGE_CHOICES)
+	hours_per_week = models.DecimalField(max_digits=4,decimal_places=2)
 
 	def clean(self):
 		if self.term == 'CF' or self.term == 'CP':
@@ -41,14 +50,3 @@ class Job(models.Model):
 
 	def __unicode__(self):
 		return self.title
-
-	class Meta:
-		abstract = True
-
-class SalariedJob(Job):
-	scale_start = models.DecimalField(max_digits=8, decimal_places=2)
-	scale_end = models.DecimalField(max_digits=8, decimal_places=2)
-	hours = models.DecimalField(max_digits=4, decimal_places=2)
-
-class NonSalariedJob(Job):
-	wage = models.DecimalField(max_digits=4, decimal_places=2)
