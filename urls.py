10d42ae1ab1023f8790d79716927eb553d83e5dd
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-29 14:35:42 -0500

    Add DetailView URL

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from job_postings.views import JobList
+from job_postings.views import JobList, JobDetail
 
 urlpatterns = [
+    url(r'^job/(?P<title>\w)/$', JobDetail.as_view()),
     url(r'^$', JobList.as_view()),
 ]
