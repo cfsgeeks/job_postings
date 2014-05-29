762e6f6eb01725e96d9d4630a9988d626a5050cf
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-29 14:42:46 -0500

    ?

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from job_postings.views import JobList, JobDetail
 
 urlpatterns = [
+    url(r'^job/\w+/$', JobDetail.as_view()),
     url(r'^$', JobList.as_view()),
-    url(r'^job/\w/$', JobDetail.as_view()),
 ]
