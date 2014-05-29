9de4d0e52aa03ba86041228353ab6a6cf6f01acb
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-29 14:28:56 -0500

    DetailView

--- views.py
+++ views.py
@@ -1,8 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from job_postings.models import Job
 import datetime
 
 TODAY = datetime.datetime.today()
 
 class JobList(ListView):
+    queryset = Job.objects.filter(closing_date__gte=TODAY)
+
+class JobDetail(DetailView):
+    model = Job
     queryset = Job.objects.filter(closing_date__gte=TODAY)
