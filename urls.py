9229f00e6416dac6e39fa25f1ad48f9d3f59cc75
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-29 14:38:51 -0500

    .

--- urls.py
+++ urls.py
@@ -3,5 +3,5 @@ from job_postings.views import JobList, JobDetail
 
 urlpatterns = [
     url(r'^$', JobList.as_view()),
-    url(r'^job/(?P<title>\w)/$', JobDetail.as_view()),
+    url(r'^job/\w/$', JobDetail.as_view()),
 ]
