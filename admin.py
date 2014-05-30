78f597e10e57d4f5f4c7c54343ef873795853c5c
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-30 10:48:39 -0500

    Add a better display of jobs

--- admin.py
+++ admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from job_postings.models import Job, Location
 
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("title","location","closing_date")
+
+admin.site.register(Job,JobAdmin)
 admin.site.register(Location)
