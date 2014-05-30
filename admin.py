127fe7e0b2b1e50893c273205aeebb6bf9b2c8f0
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-30 07:40:56 -0500

    Revert "Editor areas?"
    This reverts commit 668ca997fe0e04d469a3aa816a9cf651ec1e49a4.

--- admin.py
+++ admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from django import forms
 from job_postings.models import Job, Location
-
-class JobAdmin(admin.ModelAdmin):
-    formfield_overrides = { models.TextField: {'widget': forms.Textarea(attrs={'class':'mceEditor'})}, }
 
 admin.site.register(Job)
 admin.site.register(Location)
