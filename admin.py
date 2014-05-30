668ca997fe0e04d469a3aa816a9cf651ec1e49a4
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-30 07:39:33 -0500

    Editor areas?

--- admin.py
+++ admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django import forms
 from job_postings.models import Job, Location
+
+class JobAdmin(admin.ModelAdmin):
+    formfield_overrides = { models.TextField: {'widget': forms.Textarea(attrs={'class':'mceEditor'})}, }
 
 admin.site.register(Job)
 admin.site.register(Location)
