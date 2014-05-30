377f80d1a8845750d54d1c8be9228962a576b967
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-30 10:53:13 -0500

    .

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from job_postings.models import Job, Location
 
 class JobAdmin(admin.ModelAdmin):
-    list_display = ("title","location__name","closing_date")
+    list_display = ("title","closing_date","status")
 
 admin.site.register(Job,JobAdmin)
 admin.site.register(Location)
