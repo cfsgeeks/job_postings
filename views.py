df6c23e92a9f650d3093bd3c2d785b0d8b18badf
Adam Doherty <adam.doherty@cfsgeeks.com>
2014-05-31 07:41:52 -0500

    Changed to exclude

--- views.py
+++ views.py
@@ -5,8 +5,8 @@ import datetime
 TODAY = datetime.datetime.today()
 
 class JobList(ListView):
-    queryset = Job.objects.filter(closing_date__gte=TODAY,status='P')
+    queryset = Job.objects.exclude(closing_date__lte=TODAY,status='P')
 
 class JobDetail(DetailView):
     model = Job
-    queryset = Job.objects.filter(closing_date__gte=TODAY,status='P')
+    queryset = Job.objects.exclude(closing_date__lte=TODAY,status='P')
